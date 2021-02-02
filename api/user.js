816e7bf13cc98afef7014d9406b7b607cd2d181c
Khubaib Waheed <33741348+khubaibwaheed@users.noreply.github.com>
2021-02-02 08:29:21 +0500

    a

--- api/user.js
+++ api/user.js
@@ -1,97 +1,82 @@
 const express = require('express');
 const router = express.Router();
 const conn = require('../db');
+const md5 = require('md5');
 
-router.get('/all',(req,res)=>{
-conn.query('select * from user', (err,row)=>{
-if(err){
-    res.send({
-        status:'400',
-        user:[],
-        message:err
-    })
 
-}
 
+
+//login api
+router.post('/login?',(req,res)=>{
+
+conn.query('select * from members where email = ?',[req.query.email],(err,row)=>{
 if(row.length > 0)
 {
-    res.send({
-        status:'200',
-        user:row,
-        message:'success'
-    })
-}
-});
-});
+    const pass = md5(req.query.password);
+    
+    conn.query('select * from members where email = ? and password = ?',[req.query.email,pass],(err1,row1)=>{
 
-
-router.get('/getUser?',(req,res)=>{
-
-    conn.query('select * from user where id = ?',[req.query.id],(err,row)=>{
-        if(err)
-        {
-            res.send({
-                status: '400',
-                user:[],
-                message:err
-            })
-        }
-
-        if(row.length > 0)
-        {
+        if(row1.length>0){
+            conn.query('update members set status = 0 where email = ?',[req.query.email],(err1,row2)=>{
+            
             res.send({
                 status:'200',
-                user:row,
-                message:'Success'
+                message:'loggin',
+                user:row1
             })
-        }
-        else
-        {
-            res.send({
-                status:'200',
-                user:[],
-                message:'No Record Found'
-            })
-        }
-    })
-
-
-});
-
-
-router.get('/del?',(req,res)=>{
-
-    conn.query('select * from user where id = ?',(req.query.id),(err,row)=>{
-      
-        if(row.length > 0)
-        {
-            conn.query('delete from user where id = ?',(req.query.id),(err)=>{
-                if(err)
-                {
-                    res.send({
-                        status:'400',
-                        message:'Error: '+err
-                    })
-                }
-                else
-                {
-                    res.send({
-                        status:'200',
-                        message:'Deleted'
-                    })
-                }
         })
-        
-        }
-        else
-        {
+        }else{
             res.send({
                 status:'400',
-                message:'No Data Exist'
+                message:'password invalid'
             })
         }
+
     })
+}
+else{
+    conn.query('select * from members where username = ?',[req.query.email],(err,row)=>{
+        if(row.length > 0)
+        {
+            const pass = md5(req.query.password);
+            
+            conn.query('select * from members where username = ? and password = ?',[req.query.email,pass],(err1,row1)=>{
+        
+                if(row1.length>0){
+                    
+                    conn.query('update members set status = 0 where username = ?',[req.query.email],(err1,row2)=>{
+            
+                        res.send({
+                            status:'200',
+                            message:'loggin',
+                            user:row1
+                        })
+                    })
+                }else{
+                    res.send({
+                        status:'400',
+                        message:'password invalid'
+                    })
+                }
+        
+            })
+        }else{
+
+            res.send({
+                status:'400',
+                message:'email and password not exist'
+            })  
+        }
+
+})
+
+}
+})
+
 });
+//end
+
+
 
 
 
